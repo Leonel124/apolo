e16fb06b48e1a3190dd00697de8d5a78e3865af8
Leonel López Sánchez <leonel.lopez@webstage.com.mx>
2023-05-20 15:00:32 -0600

    Example of a database

--- school.sql
+++ school.sql
@@ -1,30 +1,54 @@
-
 USE school;
 
-CREATE TABLE if NOT exists students(
-	id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	name VARCHAR(255),  
-	lastname VARCHAR(255),
-	email VARCHAR(255),
-	password varchar(255), 
+DROP TABLE IF EXISTS users;
+DROP TABLE IF  EXISTS students;
+DROP TABLE IF EXISTS teachers;
+
+CREATE  TABLE IF NOT EXISTS users(
+	id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+   `name` VARCHAR(255) NOT NULL,
+   lastname VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL,
+   `password` VARCHAR(255) NOT NULL,
+   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP  
+);
+
+CREATE TABLE IF NOT EXISTS teachers(
+	id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+	`rank` VARCHAR(255) NOT NULL DEFAULT 'Prof.', 
+	user_id INT(11) NOT NULL,
 	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-USE school; 
+CREATE TABLE IF NOT EXISTS students(
+   id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+   grade VARCHAR(255) NOT NULL,
+   `group` VARCHAR(255) NOT NULL,
+   user_id INT(11) NOT NULL,
+   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+-- 
+
+INSERT INTO users(NAME, lastname, email, PASSWORD) VALUES("Leonel", "Bermudez", "leonel@gmail.com", "apolo12");
+INSERT INTO users(NAME, lastname, email, PASSWORD) VALUES("Mauricio", "Lopez", "mauricio@gmail.com", "mauricio12");
+SELECT * FROM users;
+
+INSERT INTO students(grade, `group`, user_id) VALUES("6to", "A", 1);
+
+
+SELECT * FROM students;
+
+INSERT INTO teachers(rank, user_id) VALUES("Director", 2);
+SELECT * FROM teachers;
+
+SELECT * FROM users a INNER JOIN students b ON a.id = b.user_id;
+SELECT * FROM users c INNER JOIN teachers d ON c.id = d.user_id;
+SELECT * FROM users a 
+LEFT OUTER JOIN students b 
+ON a.id = b.user_id
+WHERE b.user_id IS NOT NULL ;
+
+SELECT * FROM users a LEFT OUTER JOIN teachers b ON a.id = b.user_id WHERE b.user_id IS NULL;
 
 SHOW TABLES;
 
-DESC students;
-
-SELECT *  FROM students;
-
-INSERT INTO students (NAME, lastname, email,password) VALUES('leo','lopez','leo@gmail.com','123456789');
-INSERT INTO students (NAME, lastname, email, PASSWORD) VALUES('maya', 'bermudez', 'maya@gmail.com', '58772118');
-
-UPDATE students
-SET NAME = 'sigartha'
-WHERE NAME = 'apolo';
-
-DELETE FROM students WHERE id = 1 ;
-
-SELECT * FROM students;
